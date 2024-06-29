3b4b11576f2c2176908be6f52a423a155d4cbce5
Xiang511 <toby91511@gmail.com>
2024-06-29 20:28:50 +0800

    feat: nav

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
+let menuBtn = document.querySelectorAll(".icon");
+for (var i = 0; i < menuBtn.length; i++) {
+    menuBtn[i].addEventListener("click", (e) => {
+        console.log(e.target);
+        e.target.classList.toggle("active");
+    });
+}
+
+
+
 let table = new DataTable('#myTable', {
     // order: [[2, 'asc']],
     info: false,
