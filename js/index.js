82a7d9e30cc2a6c4e0ce1091a006be10705d847e
Xiang511 <toby91511@gmail.com>
2024-06-29 19:58:04 +0800

    feat: Assault_rifles page

---
+++ js/index.js
@@ -0,0 +1,46 @@
+let table = new DataTable('#myTable', {
+    // order: [[2, 'asc']],
+    info: false,
+    // paging: false
+    processing: true,
+    // 表格拖拉
+    colReorder: true,
+    colReorder: {
+        fixedColumnsLeft: 1
+    },
+    fixedColumns: {
+        start: 1,
+        end: 2
+    },
+    scrollCollapse: true,
+    scrollX: true,
+    scrollY: 500,
+    columnDefs: [{ orderable: false, targets: 0 },
+    ]
+});
+
+if (window.innerWidth > 1500) {
+
+    table.destroy();
+    
+    table = new DataTable('#myTable', {
+        // order: [[2, 'asc']],
+        info: false,
+        // paging: false
+        processing: true,
+        // 表格拖拉
+        colReorder: true,
+        colReorder: {
+            fixedColumnsLeft: 1
+        },
+        fixedColumns: {
+            start: 1,
+            end: 2
+        },
+        // scrollCollapse: true,
+        // scrollX: true,
+        // scrollY: 800,
+        columnDefs: [{ orderable: false, targets: 0 },
+        ]
+    });
+}
