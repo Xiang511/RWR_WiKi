095f8abb0e7783c38c25623f93504185e2207774
Xiang511 <toby91511@gmail.com>
2024-07-01 05:02:15 +0800

    feat: :sparkles: Comparison tool
    Now you can easily compare the stats of two guns side-by-side, without affecting the original search function.

--- js/index.js
+++ js/index.js
@@ -32,10 +32,12 @@ let table = new DataTable('#myTable', {
     scrollY: 500,
     columnDefs: [
         { orderable: false, targets: 0 },
-        { orderable: false, targets: 1 },
         { orderable: false, targets: 2 },
         { orderable: false, targets: 3 },
-    ]
+    ],
+    "language": {
+        "searchPlaceholder": "single keyword"
+    }
 });
 
 if (window.innerWidth > 1500) {
@@ -61,9 +63,49 @@ if (window.innerWidth > 1500) {
         // scrollY: 800,
         columnDefs: [
             { orderable: false, targets: 0 },
-            { orderable: false, targets: 1 },
             { orderable: false, targets: 2 },
             { orderable: false, targets: 3 },
-        ]
+        ],
+        "language": {
+            "searchPlaceholder": "single keyword"
+        }
     });
+}
+
+const searchitem1 = document.getElementById("searchitem1");
+const searchitem2 = document.getElementById("searchitem2");
+
+function multiSearch() {
+
+    $(document).ready(function () {
+        var table = $('#myTable').DataTable(); // 確保這裡的表格ID為'myTable'
+
+        $.fn.dataTable.ext.search.push(
+            function (settings, data, dataIndex) {
+                var name = data[1].toLowerCase();
+
+                if (name.includes(searchitem1.value.toLowerCase()) || name.includes(searchitem2.value.toLowerCase())) {
+                    return true;
+                }
+                return false;
+            }
+        );
+
+        table.draw();
+    });
+
+}
+
+function clearSearch() {
+
+
+    searchitem1.value = '';
+    searchitem2.value = '';
+
+    console.log(123);
+    // 移除最後一個添加的自定義搜索函數
+    $.fn.dataTable.ext.search.pop();
+
+    // 重新繪製表格以應用更改
+    $('#myTable').DataTable().draw();
 }
